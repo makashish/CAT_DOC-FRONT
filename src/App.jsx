05e69347a318f0eaa5d386cc18bd16acf49d674f
makanindian <makanindian@gmail.com>
2025-07-25 12:49:50 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import { getConfig } from "./config";
 
-const App = () => (
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </Router>
-);
+const App = () => {
+  const [backendURL, setBackendURL] = useState('');
+
+  useEffect(() => {
+    getConfig().then(config => {
+      setBackendURL(config.BACKEND_URL);
+    }).catch(error => {
+      console.error("Failed to load config.json", error);
+    });
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home backendURL={backendURL} />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
